3f57717b917548d877adf38dc88c83242c1e1dd3
theophile <chmeredith@gmail.com>
2021-01-20 17:02:10 -0600

    Fix submenu initialization

--- rpilcdmenu/rpi_lcd_submenu.py
+++ rpilcdmenu/rpi_lcd_submenu.py
@@ -1,12 +1,12 @@
 from rpilcdmenu import RpiLCDMenu
-
+from rpilcdmenu.rpi_lcd_hwd import RpiLCDHwd
 
 class RpiLCDSubMenu(RpiLCDMenu):
     def __init__(self, base_menu):
         """
         Initialize SubMenu
         """
-        self.lcd = base_menu.lcd
+        self.lcd = RpiLCDHwd()
         self.scrolling_menu = base_menu.scrolling_menu
         self.lcd_queue = base_menu.lcd_queue
 
