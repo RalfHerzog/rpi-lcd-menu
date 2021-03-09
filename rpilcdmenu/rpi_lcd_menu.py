d0b41301ca70e1d2e7c68c5d87781be1164fbd0e
theophile <chmeredith@gmail.com>
2021-03-08 19:10:01 -0600

    rpi_lcd_menu.py: Code cleanup and documentation

--- rpilcdmenu/rpi_lcd_menu.py
+++ rpilcdmenu/rpi_lcd_menu.py
@@ -3,63 +3,92 @@ import threading
 from time import sleep
 from RPLCD.i2c import CharLCD
 from RPLCD.codecs import A02Codec as LCDCodec
-
 from rpilcdmenu.base_menu import BaseMenu
 
+
 class RpiLCDMenu(BaseMenu):
+    """ Class to create and set up the main menu object. """
+
     def __init__(self, scrolling_menu=True):
         """
-        Initialize menu
+        Initialize the menu. Communication with the LCD hardware is
+        handled by the RPLCD module. By default, if the text of the current
+        menu item is longer than the display width, it will scroll right-to-
+        left, pause momentarily, and then repeat. Set 'scrolling_menu' to
+        False to disable this animation and simply truncate the extra
+        characters.
         """
-        self.lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dotsize=8, auto_linebreaks=True)
+        self.lcd = CharLCD(
+            i2c_expander="PCF8574",
+            address=0x27,
+            port=1,
+            cols=16,
+            rows=2,
+            dotsize=8,
+            auto_linebreaks=True,
+        )
+        self.lcd_framerate = 0.05  # Interval to listen for input during menu scrolling
+        self.cursor_char = ">"  # Character for menu selector
+
         self.scrolling_menu = scrolling_menu
         self.lcd_queue = queue.Queue(maxsize=0)
-        self.maxWidth = 15
-        self.lcdFrameRate = 0.05
-        self.cursor_char = '\x07'
+        self.max_width = 15
 
-        threading.Thread(target=self.lcd_queue_processor).start()
+        threading.Thread(target=self._lcd_queue_processor).start()
 
-        super(self.__class__, self).__init__()
+        super().__init__()
 
     def custom_character(self, loc, char):
+        """
+        Define a custom display character in LCD CG-RAM.
+        loc: The CG-RAM location to which to write (e.g. '\x03')
+        char: A tuple containing the bitmap representing the character
+        For more info, see:
+        https://rplcd.readthedocs.io/en/stable/usage.html#creating-custom-characters
+        """
         self.lcd.create_char(loc, char)
         return self
 
-    def home_lcd(self, var=None):
+    def home_lcd(self):
+        """ Reset LCD cursor to starting position. """
         self.lcd.home()
-        return
 
     def write_to_lcd(self, framebuffer):
+        """
+        Method to write out the formatted framebuffer to the LCD.
+        framebuffer: A list whose elements are the strings to be written to the LCD
+        """
         for row in framebuffer:
-#            self.lcd_queue.put((self.lcd.write_string, row.ljust(16)[:16]))
-#            self.lcd_queue.put((self.lcd.write_string, '\r\n'))
             self.lcd.write_string(row.ljust(16)[:16])
-            self.lcd.write_string('\r\n')
-
+            self.lcd.write_string("\r\n")
         return self
 
-    def message(self, text, autoscroll=False, clear=True):
+    def message(self, text, clear=True):
+        """
+        Method to display a static message on the LCD.
+        text: String containing the message text to be displayed
+        clear: If false, will not clear the display first
+        """
         if clear:
             self.lcd.clear()
-        if type(text) == list:
+        if isinstance(text, list):
             self.lcd_queue.put((self.write_to_lcd, text))
-#            self.write_to_lcd(text)
         else:
             self.lcd_queue.put((self.lcd.write_string, text))
-            self.lcd_queue.put((self.home_lcd,'None'))
-#            self.lcd_queue.put((self.lcd.write_string, '\r\n'))
-#            self.lcd_queue.put((self.lcd.write_string, '\r\n'))
-#            self.lcd.write_string(text)
-
+            self.lcd_queue.put((self.home_lcd, "None"))
         return self
 
     def render(self):
+        """
+        Pre-format the menu elements to be displayed. The elements are
+        then fed either to _menu_static if the menu's 'scrolling_menu'
+        attribute is False, or to _menu_scroller if True.
+        """
         if len(self.items) == 0:
-            self.message('Menu is empty')
+            self.message("Menu is empty")
             return self
 
-        elif len(self.items) <= 2:
+        if len(self.items) <= 2:
             text = [self.items[0].text, ""]
             cursor_pos = self.current_option
             if len(self.items) == 2:
@@ -74,75 +103,84 @@ class RpiLCDMenu(BaseMenu):
                 text[1] = self.items[0].text
 
         if self.scrolling_menu:
-#            self.menu_scroller(text, cursor_pos, self.input_count)
-            self.lcd_queue.put((self.menu_scroller, text, cursor_pos, self.input_count))
+            self.lcd_queue.put(
+                (self._menu_scroller, text, cursor_pos, self.input_count)
+            )
         else:
-#            self.menu_static(text, cursor_pos)
-            self.lcd_queue.put((self.menu_static, text, cursor_pos))
+            self.lcd_queue.put((self._menu_static, text, cursor_pos))
         return self
 
-    def menu_static(self, text, cursor_pos):
+    def _menu_static(self, text, cursor_pos):
+        """
+        Receive pre-formatted menu elements from render, generate final
+        framebuffer for a fixed, non-scrolling menu with indicator cursor,
+        and send it to write_to_lcd.
+        """
         inactive_row = int(cursor_pos == 0 and "1" or "0")
-        print('STATIC DISPLAY')
-        print('cursor_pos: ' + str(cursor_pos))
-        print('inactive_row: ' + str(inactive_row))
-        framebuffer = ["",""]
-        framebuffer[cursor_pos] = self.cursor_char + text[cursor_pos][:self.maxWidth]
-        framebuffer[inactive_row] = ' ' + text[inactive_row][:self.maxWidth]
+        print("STATIC DISPLAY")
+        print("cursor_pos: " + str(cursor_pos))
+        print("inactive_row: " + str(inactive_row))
+        framebuffer = ["", ""]
+        framebuffer[cursor_pos] = self.cursor_char + text[cursor_pos][: self.max_width]
+        framebuffer[inactive_row] = " " + text[inactive_row][: self.max_width]
         print(framebuffer)
         self.write_to_lcd(framebuffer)
         return self
 
-    def menu_scroller(self, text, cursor_pos, startInputCount):
+    def _menu_scroller(self, text, cursor_pos, start_input_count):
+        """
+        Receive pre-formatted menu elements from render, generate frames
+        to animate a scrolling menu with indicator cursor, and send each
+        frame of the animated menu to write_to_lcd.
+        """
         inactive_row = int(cursor_pos == 0 and "1" or "0")
-        print('SCROLLING DISPLAY')
-        print('cursor_pos: ' + str(cursor_pos))
-        print('inactive_row: ' + str(inactive_row))
-        if len(text[cursor_pos]) <= self.maxWidth:
+        print("SCROLLING DISPLAY")
+        print("cursor_pos: " + str(cursor_pos))
+        print("inactive_row: " + str(inactive_row))
+        if len(text[cursor_pos]) <= self.max_width:
             # Selected row of the menu fits on line; no need to scroll,
             # but we're going to truncate the bottom line if it's too long
-            framebuffer = ["",""]
+            framebuffer = ["", ""]
             framebuffer[cursor_pos] = self.cursor_char + text[cursor_pos]
-            framebuffer[inactive_row] = ' ' + text[inactive_row][:self.maxWidth]
+            framebuffer[inactive_row] = " " + text[inactive_row][: self.max_width]
             print(framebuffer)
             self.write_to_lcd(framebuffer)
             return self
 
         # top line too long.. so animate until there's another input event
-        aniPosition = 0
-        while startInputCount == self.input_count:
-            ### render partial menu text
-            aniText = text[cursor_pos][aniPosition: aniPosition + self.maxWidth]
-            # prepend cursor character in front of top menu item, blank space in front of bottom
-            framebuffer = ["",""]
-            framebuffer[cursor_pos] = self.cursor_char + aniText[:self.maxWidth]
-            framebuffer[inactive_row] = ' ' + text[inactive_row][:self.maxWidth]
+        ani_pos = 0
+        while start_input_count == self.input_count:
+            # Render partial menu text
+            ani_text = text[cursor_pos][ani_pos : ani_pos + self.max_width]
+            # Prepend cursor character in front of top menu item, pad bottom item
+            framebuffer = ["", ""]
+            framebuffer[cursor_pos] = self.cursor_char + ani_text[: self.max_width]
+            framebuffer[inactive_row] = " " + text[inactive_row][: self.max_width]
             # Send the framebuffer to the LCD
             self.write_to_lcd(framebuffer)
-            print(framebuffer)
-            ### determine next state
-            if aniPosition == 0 or aniPosition == len(text[cursor_pos]) - self.maxWidth:
-                delayFrames=25
+            # Determine next animation state
+            if ani_pos in (0, len(text[cursor_pos]) - self.max_width):
+                delay_frames = 25
             else:
-                delayFrames=5
-
-            aniPosition += 1
+                delay_frames = 5
+            ani_pos += 1
             # Restart the animation once the whole row has been scrolled
-            if aniPosition >= (len(text[cursor_pos]) - self.maxWidth + 1):
-                aniPosition = 0
+            if ani_pos >= (len(text[cursor_pos]) - self.max_width + 1):
+                ani_pos = 0
 
-            for _ in range(delayFrames):
-                sleep(self.lcdFrameRate)
-                if startInputCount != self.input_count:
+            # Watch for input during "pause" so interface remains responsive
+            for _ in range(delay_frames):
+                sleep(self.lcd_framerate)
+                if start_input_count != self.input_count:
                     break
-#        self.lcd.clear()
         return self
 
-    def lcd_queue_processor(self):
-        # clear it once in case of existing corruption
+    def _lcd_queue_processor(self):
+        """
+        Process all LCD write commands through a queue to prevent
+        display corruption.
+        """
         self.lcd.clear()
-
-        # process the queue
         while True:
             items = self.lcd_queue.get()
             func = items[0]
